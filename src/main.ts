91581bbdf6cdd68ef92e65a006cfdf8ead487930
Philipp Melab <philipp.melab@gmail.com>
2026-06-16 09:54:25 +0200

    refactor: remove setup subcommand

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,6 @@ import { Effect } from "effect"
 import { GitService } from "./Git.js"
 import { detect } from "./State.js"
 import { buildPrompt } from "./Prompt.js"
-import { buildSetupPrompt } from "./Setup.js"
-
-const subcommand = process.argv[2]
-
-if (subcommand === "setup") {
-  process.stdout.write(buildSetupPrompt())
-  process.exit(0)
-} else if (subcommand !== undefined) {
-  process.stderr.write(`gtd: unknown subcommand '${subcommand}'\n`)
-  process.stderr.write(`usage: gtd [setup]\n`)
-  process.exit(1)
-}
 
 const program = Effect.gen(function* () {
   const state = yield* detect()
